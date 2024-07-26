22e7293c887377e5ff94b41c53ff7feff066a88e
Nauman-Rehman <155551360+Nauman-Rehman@users.noreply.github.com>
2024-07-26 16:54:23 +0530

    Update operatorOverloading.cpp

--- lec43_OOPs_Day2/operatorOverloading.cpp
+++ lec43_OOPs_Day2/operatorOverloading.cpp
@@ -31,3 +31,34 @@ int main ()
     obj1 + obj2;
  return 0;
 }
+
+// another example of operator overloading
+# include <iostream>
+# include <string.h>
+using namespace std;
+
+class A {
+    public:
+        int a;
+        string b;
+    
+    public:
+        void operator* (A &obj){
+            int value1 = this -> a;
+            int value3 = obj.a;
+            string value2 = this -> b;
+            string value4 = obj.b;
+            cout<< "output: "<< value3 - value1<<endl;
+            cout<< "output of sring: "<< value2 + value4;
+        }
+};
+
+int main(){
+    A obj1, obj2;
+    obj1.a = 3;
+    obj2.a = 7;
+    obj1.b = "man";
+    obj2.b = "salman";
+    
+    obj1 * obj2;
+}
