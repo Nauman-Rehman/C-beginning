449cb37c3ec1915ee0bb08c30049f0de58b7ab69
Nauman-Rehman <155551360+Nauman-Rehman@users.noreply.github.com>
2024-07-29 02:25:43 +0530

    Update straight.cpp

--- LinkedList/add_numbers_represent_by_LL/straight.cpp
+++ LinkedList/add_numbers_represent_by_LL/straight.cpp
@@ -1,3 +1,6 @@
+// You are given two non-empty linked lists representing two non-negative integers. 
+// The digits are stored in reverse order, and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     ListNode* head = new ListNode(0);
     ListNode* tail = head;
